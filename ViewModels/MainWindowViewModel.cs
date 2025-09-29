c66e2588ad203361209394c4369dd91976f37fd6
jan-mski <36985396+jan-mski@users.noreply.github.com>
2025-09-29 20:14:24 +0200

    Very rough first version

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Pomidoras.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase {
+
+    public string Greeting { get; } = "25:00";
+
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Pomidoras.ViewModels;
+
+public class ViewModelBase : ObservableObject {
+
+}
