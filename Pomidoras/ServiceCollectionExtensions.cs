93017e95a63869c297461b03ca367bc0a6496564
jan-mski <36985396+jan-mski@users.noreply.github.com>
2025-11-09 16:59:26 +0100

    Add dependency injection

---
+++ Pomidoras/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pomidoras.ViewModels;
+
+namespace Pomidoras;
+
+public static class ServiceCollectionExtensions
+{
+
+    public static void AddServices(this IServiceCollection collection)
+    {
+        collection.AddTransient<MainWindowViewModel>();
+    }    
+
+}
