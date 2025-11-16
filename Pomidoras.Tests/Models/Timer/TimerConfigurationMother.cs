13eb7038be4dfe38793b91dcaa0c97137e1f8e12
jan-mski <36985396+jan-mski@users.noreply.github.com>
2025-11-16 20:03:55 +0100

    Implement timer configuration (#2)

---
+++ Pomidoras.Tests/Models/Timer/TimerConfigurationMother.cs
@@ -0,0 +1,34 @@
+﻿using Pomidoras.Models.Timer;
+
+namespace Pomidoras.Tests.Models.Timer;
+
+public static class TimerConfigurationMother
+{
+    
+    public static readonly TimeSpan DefaultWorkDuration = TimeSpan.FromMinutes(25);
+    public static readonly TimeSpan DefaultBreakShortDuration = TimeSpan.FromMinutes(5);
+    public static readonly TimeSpan DefaultBreakLongDuration = TimeSpan.FromMinutes(15);
+    public static readonly TimeSpan DefaultInterval = TimeSpan.FromSeconds(1);
+    public const TimerMode DefaultDefaultMode = TimerMode.Work;
+    public const int DefaultWorkSessionsUntilBreakLong = 4;
+
+    public static TimerConfiguration Default()
+    {
+        return new TimerConfiguration(
+            DefaultWorkDuration,
+            DefaultBreakShortDuration,
+            DefaultBreakLongDuration,
+            DefaultInterval,
+            DefaultDefaultMode,
+            DefaultWorkSessionsUntilBreakLong
+        );
+    }
+
+    // I could use this for public properties, but for private ones I'll need to use the constructor
+    // and store values in static constants.
+    // public static TimerConfiguration WithManyWorkSessions()
+    // {
+    //     return Default() with { WorkSessionsUntilBreakLong = 10 };
+    // }
+
+}
---
+++ Pomidoras.Tests/Models/Timer/TimerConfigurationTest.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Pomidoras.Models.Timer;
+
+namespace Pomidoras.Tests.Models.Timer;
+
+public class TimerConfigurationTest
+{
+
+    public static TheoryData<TimerMode, TimeSpan> GetTimerModeAndExpectedDurations()
+    {
+        return new TheoryData<TimerMode, TimeSpan>
+        {
+            { TimerMode.Work, TimerConfigurationMother.DefaultWorkDuration },
+            { TimerMode.BreakShort, TimerConfigurationMother.DefaultBreakShortDuration },
+            { TimerMode.BreakLong, TimerConfigurationMother.DefaultBreakLongDuration }
+        };
+    }
+
+    [Fact]
+    public void Constructor_InitializesProperties_Correctly()
+    {
+        var configuration = TimerConfigurationMother.Default();
+
+        var expectedInterval = TimerConfigurationMother.DefaultInterval;
+        var expectedDefaultMode = TimerConfigurationMother.DefaultDefaultMode;
+        var expectedWorkSessionsUntilBreakLong = TimerConfigurationMother.DefaultWorkSessionsUntilBreakLong;
+
+        configuration.Interval.Should().Be(expectedInterval);
+        configuration.DefaultMode.Should().Be(expectedDefaultMode);
+        configuration.WorkSessionsUntilBreakLong.Should().Be(expectedWorkSessionsUntilBreakLong);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTimerModeAndExpectedDurations))]
+    public void GetDuration_ReturnsCorrectDuration_ForValidTimerMode(TimerMode timerMode, TimeSpan expectedDuration)
+    {
+        var configuration = TimerConfigurationMother.Default();
+
+        var actualDuration = configuration.GetDuration(timerMode);
+
+        actualDuration.Should().Be(expectedDuration);
+    }
+
+    [Fact]
+    public void GetDuration_ThrowsArgumentException_ForInvalidTimerMode()
+    {
+        var configuration = TimerConfigurationMother.Default();
+        var invalidTimerMode = (TimerMode)999;
+
+        var act = () => configuration.GetDuration(invalidTimerMode);
+
+        act.Should().Throw<ArgumentException>()
+            .WithParameterName("timerMode");
+    }
+
+}
