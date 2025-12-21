d77a5b9e9d3055fd68a2644bb047677382804e71
jan-mski <36985396+jan-mski@users.noreply.github.com>
2025-12-21 21:21:55 +0100

    add timer controls

---
+++ Pomidoras/Controls/TimerPlaybackControl.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+
+namespace Pomidoras.Controls;
+
+public partial class TimerPlaybackControl : UserControl
+{
+
+    public TimerPlaybackControl()
+    {
+        InitializeComponent();
+    }
+
+}
